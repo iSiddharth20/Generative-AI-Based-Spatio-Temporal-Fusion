2d57423084e9a2a3adbf7f0da5eaea5104b6e164
iSiddharth20 <kekresiddharth20@gmail.com>
2023-12-04 05:22:53 -0800

    Updated main.py module

--- Code/main.py
+++ Code/main.py
@@ -1,17 +1,75 @@
 '''
-Importing Individual Components
+Need to Combine Auto-Encoder and LSTM
+    The Auto-Encoder is created using Encoder and Decoder Classes
+    The LSTM Network is obtained from the LSTMModule Class
+    The Training Loss in obtained from the CompositeLossFunction Class
+--------------------------------------------------------------------------------
+This Code Needs to be Modified based on the Problem Statement 
 '''
+
+# Importing Necessary Libraries and Individual Components
+
+import torch
+import torch.optim as optim
+
+from LossFunction import CompositeLossFunction
 from Encoder import Encoder
 from Decoder import Decoder
-from LSTM import LSTM
+from LSTM import LSTMModule
 
 
-'''
-Need to Combine Encoder, LSTM and Decoder
-    This Method creates the AutoEncoder Model with LSTM Network in its Hidden Layers
-    The Encoder and Decoder are created using the Encoder and Decoder Classes
-    The LSTM Network is created using the LSTM Class
-    The Combined Model is Returned
-'''
-def EncoderLSTMDecoder():
-    return None
+# Define the AutoEncoder class as a combination of Encoder and Decoder
+class AutoEncoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(AutoEncoder, self).__init__()
+        # Initialize the encoder and decoder using the given dimensions
+        self.encoder = Encoder(input_size, hidden_size)
+        self.decoder = Decoder(hidden_size, output_size)
+
+    def forward(self, x):
+        # Pass Data through Encoder
+        x = self.encoder(x)
+        # Pass Encoded Data through Decoder
+        x = self.decoder(x)
+        return x
+
+# Initialize the model
+input_size = 000  
+hidden_size = 000  
+output_size = 000  
+autoencoder = AutoEncoder(input_size, hidden_size, output_size)
+
+# Initialize the Composite Loss Function (Alpha may need tuning)
+loss_function = CompositeLossFunction(alpha=0.5) 
+
+# Initialize Adam Optimizer
+optimizer = optim.Adam(autoencoder.parameters(), lr=0.001)
+
+# Import Dataset
+dataloader = 'Import Dataset Here'
+
+# Training loop
+num_epochs = 10 
+for epoch in range(num_epochs):
+    for data in dataloader:
+        # Load the Data
+        images, _ = data  # Ignore labels if present
+
+        # Reset gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        reconstructed_images = autoencoder(images)
+
+        # Compute loss
+        loss = loss_function(reconstructed_images, images)
+
+        # Backward pass
+        loss.backward()
+
+        # Update weights
+        optimizer.step()
+
+    # Print training progress
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+
