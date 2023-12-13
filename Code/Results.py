30519ce609c25db6360f834c3271cdb257d5649e
Siddharth Kekre <isiddharth@iSiddharth-MacBookPro.local>
2023-12-13 09:52:41 -0800

    Removed Previous Code Structure

--- Code/Results.py
+++
@@ -1,5 +0,0 @@
-'''
-Module that uses Structural Similarity Index Measure (SSIM) as validation metrics on Validation Sets
---------------------------------------------------------------------------------
-This is a Template Code and Needs to be Modified based on the Problem Statement 
-'''
